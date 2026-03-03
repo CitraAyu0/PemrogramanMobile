61d2fc33414b0222a0d225bdf0101f76fa67a1bb
CitraAyu0 <meilindacitraayu@gmail.com>
2026-03-03 15:04:49 +0700

    Menambahkan logika tebak angka dengan perulangan

--- PM-Week03/src/GuessingGame.dart
+++ PM-Week03/src/GuessingGame.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 
 void main() {
@@ -5,6 +6,18 @@ void main() {
 
   Random random = Random();
   int angkaRahasia = random.nextInt(100) + 1;
+  int tebakan = 0;
 
-  print("Angka rahasia telah dibuat!");
+  while (tebakan != angkaRahasia) {
+    stdout.write("Masukkan tebakan: ");
+    tebakan = int.parse(stdin.readLineSync()!);
+
+    if (tebakan > angkaRahasia) {
+      print("Terlalu besar!");
+    } else if (tebakan < angkaRahasia) {
+      print("Terlalu kecil!");
+    } else {
+      print("Selamat! Kamu benar!");
+    }
+  }
 }
