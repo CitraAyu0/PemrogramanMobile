0d8225cad828ee53e40a9e1fa4d36d643a1380f9
CitraAyu0 <meilindacitraayu@gmail.com>
2026-03-03 15:01:11 +0700

    Menambahkan fitur angka acak

--- PM-Week03/src/GuessingGame.dart
+++ PM-Week03/src/GuessingGame.dart
@@ -1,3 +1,10 @@
+import 'dart:math';
+
 void main() {
   print("===== GAME TEBAK ANGKA =====");
+
+  Random random = Random();
+  int angkaRahasia = random.nextInt(100) + 1;
+
+  print("Angka rahasia telah dibuat!");
 }
